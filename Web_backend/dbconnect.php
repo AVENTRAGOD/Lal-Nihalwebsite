01609b9e920086d04459adab6f77f1d706ddea34
AVENTRAGOD <152941328+AVENTRAGOD@users.noreply.github.com>
2026-03-25 12:00:00 +0530

    Initial commit - code files only

---
+++ Web_backend/dbconnect.php
@@ -0,0 +1,56 @@
+<?php
+// Database connection
+$servername = "localhost";  // Change this to your server's nameusername = "root";         // Change this to your database username
+$username = "root";
+$password = "";             // Change this to your database passworddbname = "contacts";       // Change this to your database name
+$database = "contact";
+
+// Create connection
+$conn = new mysqli($servername,$username,$password, $database);
+
+// Check connection
+//
+
+// Fetching data from the databasesql = "SELECT username, address, email, message, response FROM reach_outs";
+// result =conn->query(sql);
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
